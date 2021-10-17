d9cc97b2bec7a11326ebf2a8666529293031fe07
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-17 18:18:53 +0900

    emailとpasswordを入れるフォームのようなもの

--- src/features/auth/SignIn.tsx
+++ src/features/auth/SignIn.tsx
@@ -5,25 +5,36 @@ import { FormGroup, Input, StatusLabel } from 'smarthr-ui'
 
 const SampleStatusLabelProps: React.ComponentProps<typeof StatusLabel>[] = [
     {
-        children: 'label 1',
+        children: 'required',
         type: 'required'
-    },
-    {
-        children: 'label 2',
-        type: 'success'
     },
 ]
 
 export const SignIn: React.FC<Record<string, unknown>> = () => (
-    <FormGroup
-        title="FormGroup sample"
-        titleType="subSubBlockTitle"
-        labelId="form-group-sample"
-        innerMargin="XXS"
-        statusLabelProps={SampleStatusLabelProps}
-        helpMessage="help message text"
-        errorMessages={['error message 1', 'error message 2']}
-    >
-        <Input aria-labelledby="form-group-sample" />
-    </FormGroup>
+    <div>
+        <FormGroup
+            title="email"
+            titleType="subSubBlockTitle"
+            labelId="form-group-sample"
+            innerMargin="XXS"
+            statusLabelProps={SampleStatusLabelProps}
+            helpMessage="help message text"
+            errorMessages={[]}
+        >
+            <Input aria-labelledby="form-group-sample" />
+        </FormGroup>
+        <FormGroup
+            title="password"
+            titleType="subSubBlockTitle"
+            labelId="form-group-sample"
+            innerMargin="XXS"
+            statusLabelProps={SampleStatusLabelProps}
+            helpMessage="help message text"
+            errorMessages={[]}
+        >
+            <Input
+                aria-labelledby="form-group-sample"
+                type="password" />
+        </FormGroup>
+    </div>
 )
