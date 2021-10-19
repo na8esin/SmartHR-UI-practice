fd42dc630edb8e61d3acc2c525ab7b0ebb34e9cc
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-19 14:44:59 +0900

    ログインフォーム側だけ

--- src/features/auth/SignIn.tsx
+++ src/features/auth/SignIn.tsx
@@ -1,42 +1,41 @@
 import React, { VFC } from 'react'
 import ReactDOM from 'react-dom'
 import { FormGroup, Input, StatusLabel } from 'smarthr-ui'
-
-
-const SampleStatusLabelProps: React.ComponentProps<typeof StatusLabel>[] = [
-    {
-        children: 'required',
-        type: 'required'
-    },
-]
+import styled, { css } from 'styled-components'
 
 export const SignIn: React.FC<Record<string, unknown>> = () => (
     <div>
         <FormGroup
-            title="email"
+            title="singin"
             titleType="subSubBlockTitle"
             labelId="form-group-sample"
             innerMargin="XXS"
-            statusLabelProps={SampleStatusLabelProps}
-            helpMessage="help message text"
             errorMessages={[]}
         >
-            <Input aria-labelledby="form-group-sample" />
-            <span> </span>
-            <Input aria-labelledby="form-group-sample" />
-        </FormGroup>
-        <FormGroup
-            title="password"
-            titleType="subSubBlockTitle"
-            labelId="form-group-sample"
-            innerMargin="XXS"
-            statusLabelProps={SampleStatusLabelProps}
-            helpMessage="help message text"
-            errorMessages={[]}
-        >
-            <Input
-                aria-labelledby="form-group-sample"
-                type="password" />
-        </FormGroup>
-    </div>
+            <List>
+                <li>
+                    <Txt>email</Txt>
+                    <Input aria-labelledby="form-group-sample" />
+                </li>
+                <li>
+                    <Txt>password</Txt>
+                    <Input
+                        aria-labelledby="form-group-sample"
+                        type="password" />
+                </li>
+            </List>
+        </FormGroup >
+    </div >
 )
+
+const List = styled.ul`
+  padding: 0 24px;
+  list-style: none;
+  & > li:not(:first-child) {
+    margin-top: 16px;
+  }
+`
+
+const Txt = styled.p`
+  margin: 0 0 8px 0;
+`
